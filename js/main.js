faa0b15b0588d381b9c20ac5cfc9b812331e688a
LilGui <rafaelwilliansgi4@gmail.com>
2021-10-23 04:26:04 -0300

    vonx

--- js/main.js
+++ js/main.js
@@ -1,12 +1,47 @@
-const menu = document.getElementById("open-menu");
-const butao = document.getElementById('buu');
-
-const openii = () =>{
-    menu.style.transform = "translateX(0px)";
-    butao.setAttribute('onclick', 'fecha()')
+window.onscroll = function() {scrollFunction()};
+const logo = document.getElementById('logo');
+const cop = document.body;
+const burg = document.getElementById('burg');
+const menu = document.getElementById('box-menu');
+const sun = document.getElementById('sun');
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.height = "50px";
+    logo.style.fontSize = "2rem"
+    burg.style.top = '27px'
+    menu.style.marginTop = '50px'
+  } else {
+    document.getElementById("header").style.height = "100px";
+    logo.style.fontSize = "3rem"
+    burg.style.top = '50%'
+    menu.style.marginTop = '100px'
+  }
 }
-
-const fecha = () => {
-    menu.style.transform = "translateX(-400px)";
-    butao.setAttribute('onclick', 'openii()')
+const open_menu = () =>{
+  menu.style.transform = 'translateX(0%)';
+  burg.setAttribute('onclick', 'close_menu()');
 }
+const close_menu = () =>{
+  menu.style.transform = 'translateX(-100%)';
+  burg.setAttribute('onclick', 'open_menu()');
+}
+function artes(){
+  window.location.href = "https://www.instagram.com/voncolage/"
+}
+function web(){
+  window.location.href = "https://google.com"
+}
+function extras(){
+  window.location.href = "https://www.youtube.com/channel/UC-9Du8a1oMoORZT-bVbIPsQ"
+}
+function butt_more(){
+  window.location.href = "https://github.com/LiLGui"
+}
+// function bg_color_black() {
+//   cop.style.background = 'var(--color-bg_two)';
+//   sun.setAttribute('onclick', 'bg_color_white()');
+// }
+// const bg_color_white = () => {
+//   cop.style.background = 'var(--color-bg)';
+//   sun.setAttribute('onclick', 'bg_color_black()');
+// }
